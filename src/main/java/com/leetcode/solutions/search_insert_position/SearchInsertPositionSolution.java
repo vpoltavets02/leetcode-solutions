0a9f7d874d8fe648ed9344024ce5b58219c1b6bf
Vladyslav Poltavets <vpoltavets02@gmail.com>
2026-01-20 11:44:32 +0100

    35. Search Insert Position problem solution (19.01.2026)

---
+++ src/main/java/com/leetcode/solutions/search_insert_position/SearchInsertPositionSolution.java
@@ -0,0 +1,60 @@
+package com.leetcode.solutions.search_insert_position;
+
+/*
+https://leetcode.com/problems/search-insert-position/
+
+35. Search Insert Position
+
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+
+Personal best:
+Runtime: 0 ms, Beats 100.00%
+Memory: 44.29 MB, Beats 98.11%
+ */
+
+public class SearchInsertPositionSolution {
+
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[]{1,3,5,6}, 5));
+        System.out.println(searchInsert(new int[]{1,3,5,6}, 2));
+        System.out.println(searchInsert(new int[]{1,3,5,6}, 7));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (nums[middle] == target)
+                return middle;
+            if (nums[middle] > target) {
+                high = middle - 1;
+            } else {
+                low = middle + 1;
+            }
+        }
+        return low;
+    }
+}
